71640f577f42793218e290c1c61724f4da4997b0
zeeshandipro <65391484+zeeshandipro@users.noreply.github.com>
2020-11-10 16:39:12 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,59 @@
+<?php
+?>
+<html>
+     <head>
+	       <title>Registration Form</title>
+	 </head>
+	 <body>
+	      <h1>Registration Form</h1>
+		  <form>
+		       <table>
+			        <tr>
+					  <td>Full Name:</td>
+					  <td><input type="text"></td>
+					</tr>
+					<tr>
+					  <td>User Name:</td>
+					  <td><input type="text"></td>
+					</tr>
+					<tr>
+					  <td>Password:</td>
+					  <td><input type="password"></td>
+					</tr>
+					<tr>
+					  <td>Confirm Password:</td>
+					  <td><input type="password"></td>
+					</tr>
+					<tr>
+					  <td>Gender:</td>
+					  <td>
+					    <input type="radio" name="gender"> Male
+						<input type="radio" name="gender"> FeMale
+					  </td>
+					</tr>
+					<tr>
+					  <td>E-mail Address:</td>
+					  <td><input type="text"></td>
+					</tr>
+					<tr>
+					  <td>Contact No:</td>
+					  <td><input type="text"></td>
+					</tr>
+					<tr>
+					  <td>City:</td>
+					  <td>
+					     <select>
+						       <option>Dhaka</option>
+						 </select>
+						 <select>
+						       <option>Khulna</option>
+						 </select>
+						 <select>
+						       <option>Chittagong</option>
+						 </select>
+					  </td>
+					</tr>
+			   </table>
+		  </form>
+	 </body>
+</html>
